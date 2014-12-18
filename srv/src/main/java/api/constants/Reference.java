ccf89b9b8b52fab3674b4937dd731bc4115e247a
Briggs Thompson <briggs@rmn.com>
2014-12-17 20:12:24 -0600

    change s3 location

--- srv/src/main/java/api/constants/Reference.java
+++ srv/src/main/java/api/constants/Reference.java
@@ -6,7 +6,8 @@ import java.util.Map;
 
 public final class Reference {
     public static final String FACEBOOK_SESSION = "facebookSession";
-    public static final String IMAGE_PATH = "rentsy-images.s3.amazonaws.com/";
+    public static final String IMAGE_PATH = "images-original.s3.amazonaws.com/";
+    //public static final String IMAGE_RESIZE_PATH = "rent-images.s3.amazonaws.com/x100";
     public static final Map<String, String> validContentTypes;
     static {
         validContentTypes = new HashMap<>();
